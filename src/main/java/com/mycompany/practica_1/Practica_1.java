b8acf530b9a99564a45f1969617c0ddb1ac50c4c
Shejin12 <sergiorodriguez202131443@cunoc.edu.gt>
2024-08-18 23:38:46 -0600

    first commit

---
+++ src/main/java/com/mycompany/practica_1/Practica_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica_1;
+
+/**
+ *
+ * @author cheji
+ */
+public class Practica_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
